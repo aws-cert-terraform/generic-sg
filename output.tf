16253c472b36e7ccef66fb5cd5f373f6fcfe0ec3
Ian Cullinane <ian.cullinane@gmail.com>
2019-07-22 09:42:22 -0400

    output name

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "security_group_id" {
     value = "${aws_security_group.basic_sg.id}"
 }
+
+output "security_group_name" {
+    value = "${aws_security_group.basic_sg.security_group_name}"
+}
